240d006627cc1c08ec1a8dfda3f54d539b6f9436
Julian <jlmurraygarcia@gmail.com>
2023-02-27 04:27:43 -0800

    Wttr: Style updates

--- src/app/components/wttr-display/services/wttr.service.ts
+++ src/app/components/wttr-display/services/wttr.service.ts
@@ -1,5 +1,9 @@
 import { Injectable } from '@angular/core';
-import {Observable, tap} from "rxjs";
+import {
+  map,
+  Observable,
+  tap
+} from 'rxjs';
 import {HttpClient} from "@angular/common/http";
 
 /**
@@ -13,6 +17,7 @@ export class WttrService {
 
   private apiBaseUrl: string = 'https://wttr.in/';
   private jsonFormat: string = '?format=j1';
+  private defaultFormat: string = '?1pqQF';
 
   constructor(
     private httpClient: HttpClient
@@ -26,6 +31,16 @@ export class WttrService {
       }));
   }
 
-  getText$ = (search: string): Observable<any> => this.get$(search, 'text');
+  getText$(search: string): Observable<any> {
+    return this.get$(search + this.defaultFormat, 'text').pipe(
+      map((htmlResult: string) => {
+        // remove the search string from the start of the response text
+        if (htmlResult.includes(search))
+          htmlResult = htmlResult.replace(search, '');
+
+        return htmlResult;
+      }));
+  };
+
   getBlob$ = (search: string): Observable<any> => this.get$(search, 'blob');
 }
